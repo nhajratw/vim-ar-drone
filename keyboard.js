63dc32e6f92bf155d2d96e60cbb00c659b93c655
Nayan Hajratwala <nayan@chikli.com>
2014-01-08 08:32:45 -0500

    added more actions

--- keyboard.js
+++ keyboard.js
@@ -5,6 +5,9 @@ var stdin = process.stdin;
 
 var COMMAND_DURATION = 500;
 
+var animationsWithFlips = ['phiM30Deg', 'phi30Deg', 'thetaM30Deg', 'theta30Deg', 'theta20degYaw200deg', 'theta20degYawM200deg', 'turnaround', 'turnaroundGodown', 'yawShake', 'yawDance', 'phiDance', 'thetaDance', 'vzDance', 'wave', 'phiThetaMixed', 'doublePhiThetaMixed', 'flipAhead', 'flipBehind', 'flipLeft', 'flipRight'];
+var animations = ['phiM30Deg', 'phi30Deg', 'thetaM30Deg', 'theta30Deg', 'theta20degYaw200deg', 'theta20degYawM200deg', 'turnaround', 'turnaroundGodown', 'yawShake', 'yawDance', 'phiDance', 'thetaDance', 'vzDance', 'wave', 'phiThetaMixed', 'doublePhiThetaMixed'];
+
 // without this, we would only get streams once enter is pressed
 stdin.setRawMode( true );
 
@@ -44,23 +47,30 @@ stdin.on( 'data', function( key ){
       client.clockwise(0.5);
       stop();
       break;
+    case 'f':
+      client.front(0.5);
+      stop();
+      break;
+    case 'b':
+      client.back(0.5);
+      stop();
+      break;
     case '?':
-      var animations = ['phiM30Deg', 'phi30Deg', 'thetaM30Deg', 'theta30Deg', 'theta20degYaw200deg',
-          'theta20degYawM200deg', 'turnaround', 'turnaroundGodown', 'yawShake',
-          'yawDance', 'phiDance', 'thetaDance', 'vzDance', 'wave', 'phiThetaMixed',
-          'doublePhiThetaMixed', 'flipAhead', 'flipBehind', 'flipLeft', 'flipRight'];
       var animationIndex = Math.floor((Math.random() * animations.length) + 1); 
       console.log("Starting Animation: " + animations[animationIndex]);
       client.animate(animations[animationIndex], 1000);
       break;
     case '!':
-      client.animate('wave',1000);
-      client.after(2000, function() {
-        this.animate('yawDance', 1000);
+      animations.forEach(function(animation) {
+        client.animate(animation, 1500);
       });
-     // client.animate('doublePhiThetaMixed', 1000);
-     // client.animate('phiDance', 1000);
-     // client.animate('flipAhead', 1000);
+    case '@':
+      client.animate('wave',2000);
+      client.after(2000, function() {
+        this.animate('yawDance', 2000);
+      });
+      client.animate('doublePhiThetaMixed', 2000);
+      client.animate('phiDance', 2000);
       break;
   }
 });
